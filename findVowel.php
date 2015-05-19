2bd06d8a3c431396f9cadb48b729376123804677
padmashrimallapur <vijaymajagaonkar@trinity.local>
2015-05-19 19:26:33 -0400

    Test commit

--- findVowel.php
+++ findVowel.php
@@ -9,6 +9,7 @@ for($i=0; $i<strlen($str); $i++)
 	{	$position_arr[$i]=$str[$i]; }
 	
 }
+
 list($end) = array_keys(array_slice($position_arr, -1, 1, true)); //finding the end of an array
 print("The Vowels 'a e i o u' found in the following positions ");
 foreach($position_arr as $key=>$value)
